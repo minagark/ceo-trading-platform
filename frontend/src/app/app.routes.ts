7462847bb2741641f4cd877af5c4c16015bb44cc
Mihir Nagarkatti <mihir.nagarkatti@gmail.com>
2026-08-26 20:51:40 +0000

    style: add good colors

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,21 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
+import { Trades } from './trades/trades';
+import { TransactionHistory } from './transaction-history/transaction-history';
+import { MarketAnalysis } from './market-analysis/market-analysis';
+import { PortfolioPerformance } from './portfolio-performance/portfolio-performance';
 
 export const routes: Routes = [
   { path: 'home', component: HomePage },
-  { path: 'trades', component: HomePage },
-  { path: 'history', component: HomePage },
-  { path: 'portfolio', component: HomePage },
-  { path: 'analysis', component: HomePage },
+  { path: 'trades', component: Trades },
+  { path: 'history', component: TransactionHistory },
+  { path: 'portfolio', component: PortfolioPerformance },
+  { path: 'analysis', component: MarketAnalysis },
 ];
-
-
-
-/*
-      <a href="ceos-mn-home"><i class="lni lni-home"></i> Home</a>
-      <a href="ceos-mn-market-analysis"><i class="lni lni-home"></i> Trades</a>
-      <a href="ceos-mn-home"><i class="lni lni-home"></i> Transaction History</a>
-      <a href="ceos-mn-trades"><i class="lni lni-home"></i> Portfolio Performance</a>
-      <a href="ceos-mn-transactions"><i class="lni lni-home"></i> Market Analysis</a>
-
-*/
