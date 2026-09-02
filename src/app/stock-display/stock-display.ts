7de0a2f17f9481dda39f0b2e99272d2f30d95d04
Mihir Nagarkatti <mihir.nagarkatti@gmail.com>
2026-09-02 14:47:03 +0000

    feat: change data input to be easier to change once we find api

--- src/app/stock-display/stock-display.ts
+++ src/app/stock-display/stock-display.ts
@@ -13,8 +13,28 @@ interface PriceHistoryPoint {
   price: number;
 }
 
+// TODO: Once we get API, change this interface to match it. 
+interface StockDisplayInfo {
+  name: string;
+  symbol: string;
+  currentPrice: number;
+  previousClose: number;
+  highPriceToday?: number;
+  lowPriceToday?: number;
+  otherPriceInfo?: {[label: string]: number};
+  timeUpdated: Date;
+}
+
 @Component({
-  imports: [FormsModule, DecimalPipe, MatCardModule, MatFormFieldModule, MatSelectModule, MatButtonModule, PortfolioPerformance],
+  imports: [
+    FormsModule, 
+    DecimalPipe, 
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatButtonModule, 
+    PortfolioPerformance
+  ],
   selector: 'app-stock-display',
   styleUrl: './stock-display.css',
   templateUrl: './stock-display.html',
@@ -22,34 +42,47 @@ interface PriceHistoryPoint {
 export class StockDisplay {
   private router: Router = inject(Router);
 
+  stockDisplayInfo: StockDisplayInfo;
+  constructor() {
+    this.stockDisplayInfo = this.fetchStockInfo();
+  }
+
   // Example data, all of this comes from backend/db
-  name = 'Example Corp';
-  symbol = 'EXC';
-  currentPrice = 152.34;
-  previousClose = 149.87;
-  highPriceToday = 154.10;
-  lowPriceToday = 148.92;
-  timeFrames = ['1D', '1W', '1M', '3M', '1Y', '5Y'];
+  readonly TIME_FRAMES = ['1D', '1W', '1M', '3M', '1Y', '5Y'];
   selectedTimeFrame = '1D';
   // Example data end
 
   get priceChange(): number {
-    return this.currentPrice - this.previousClose;
+    return this.stockDisplayInfo.currentPrice - this.stockDisplayInfo.previousClose;
   }
 
   get priceChangePercent(): number {
-    return (this.priceChange / this.previousClose) * 100;
+    return (this.priceChange / this.stockDisplayInfo.previousClose) * 100;
   }
 
   priceHistory: PriceHistoryPoint[] = [];
 
+  fetchStockInfo() {
+    // Replace this with real fetch information
+    return {
+      name: "Example Corp",
+      symbol: "EXC",
+      currentPrice: 152.34,
+      previousClose: 149.87,
+      timeUpdated: new Date(),
+    }
+  }
 
   handleTrade() {
     console.log("Trading!");
     // This will likely have to be refactored into a pop-up for trades rather than
     // a navigation to a full trades page.
     this.router.navigate(["trades"], {
-      queryParams: { name: this.name, symbol: this.symbol, currentPrice: this.currentPrice }
+      queryParams: { 
+        name: this.stockDisplayInfo.name, 
+        symbol: this.stockDisplayInfo.symbol, 
+        currentPrice: this.stockDisplayInfo.currentPrice 
+      }
     });
   }
 }
