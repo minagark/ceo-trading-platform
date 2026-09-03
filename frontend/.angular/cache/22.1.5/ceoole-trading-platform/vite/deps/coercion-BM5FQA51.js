4db419b7e76c709c12dd73c7d77d1d2516f1caab
drake may <drake@may.com>
2026-09-03 13:24:16 +0000

    fixed commit?

--- frontend/.angular/cache/22.1.5/ceoole-trading-platform/vite/deps/coercion-BM5FQA51.js
+++
@@ -1,6 +0,0 @@
-//#region node_modules/@angular/cdk/fesm2022/coercion.mjs
-function coerceBooleanProperty(value) {
-	return value != null && `${value}` !== "false";
-}
-//#endregion
-export { coerceBooleanProperty as t };
